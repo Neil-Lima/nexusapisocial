e17f35c4eb6aee309099842a683d4800bbe745c9
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-12-07 12:06:55 -0300

    feed

--- frontend/src/app/feed/page.jsx
+++ frontend/src/app/feed/page.jsx
@@ -1,53 +1,7 @@
 'use client';
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { useTheme } from '@/context/theme/ThemeContext';
-import { GradientBackground, GradientText, IconWrapper } from '@/styles/GlobalStyles';
-
-// Componentes Globais
-import NavMenuComp from '@/shared/navbar/NavMenuComp';
-import ProfileCardComp from '@/shared/profile/components/ProfileCardComp';
-import StoriesComp from '@/shared/stories/StoriesComp';
-import CreatePostComp from '@/shared/post/components/CreatePostComp';
-import PostCardComp from '@/shared/post/components/PostCardComp';
-import LoadMoreComp from '@/shared/loadmore/LoadMoreComp';
-import SuggestionsFriendComp from '@/shared/suggestions/components/SuggestionsFriendComp';
-import NewsLatestComp from '@/shared/news/components/NewsLatestComp';
-import MessengerWindowComp from '@/shared/messenger/MessengerWindowComp';
-import ProfileStatsCardComp from '@/shared/profile/components/ProfileStatsCardComp';
-import MenuListComp from '@/shared/profile/components/MenuListComp';
-import ProfileVisitorsComp from '@/shared/visitors/components/ProfileVisitorsComp';
+import FeedMainComp from '@/components/feed/main/FeedMainComp';
 
 export default function FeedPage() {
-  const { theme } = useTheme();
-
-  return (
-    <>
-     <NavMenuComp />
-     <GradientBackground theme={theme}>     
-     <Container>
-       <Row>
-         <Col lg={3}>
-         <ProfileCardComp theme={theme}/>
-           <ProfileStatsCardComp theme={theme}/>
-           <MenuListComp theme={theme} /> 
-         </Col>
-         <Col lg={6}>
-         <ProfileVisitorsComp />
-           <StoriesComp />
-           <CreatePostComp />
-           <PostCardComp />
-           <LoadMoreComp />
-         </Col>
-         <Col lg={3}>
-         <SuggestionsFriendComp />
-           <NewsLatestComp />
-         </Col>
-       </Row>
-     </Container>
-     <MessengerWindowComp />
-   </GradientBackground>
-    </>
- 
-  );
+  return <FeedMainComp />;
 }
