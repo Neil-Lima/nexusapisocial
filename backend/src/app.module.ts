3d14ddfe19b896a8e7dbc51b1d56d85c56ef58d6
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-12-31 12:53:33 -0300

    l

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { CommunitiesModule } from './modules/communities/communities.module';
+import { StoriesModule } from './modules/stories/stories.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import corsConfig from './config/cors.config';
+import uploadsConfig from './config/uploads.config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
