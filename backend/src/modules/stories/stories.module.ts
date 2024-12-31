c9a3db4c285fe28e24bba50c3a1366f388a09592
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-12-31 12:45:44 -0300

    d

--- backend/src/modules/stories/stories.module.ts
+++ backend/src/modules/stories/stories.module.ts
@@ -1,15 +1,15 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StoriesService } from './services/stories.service';
 import { StoriesController } from './controllers/stories.controller';
 import { Story, StorySchema } from './schemas/story.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Story.name, schema: StorySchema },
-      { name: 'User', schema: 'UserSchema' }
+      { name: User.name, schema: UserSchema }
     ])
   ],
   controllers: [StoriesController],
