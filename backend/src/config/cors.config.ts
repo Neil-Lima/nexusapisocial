a4a9a1de86aa3c1cbaf98dcc09470861a5d426c2
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2025-01-01 20:47:18 -0300

    Update cors.config.ts

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -2,7 +2,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const corsConfig = {
-  origin: ['http://localhost:3000', 'https://nexusapisocial.netlify.app', 'https://nexusapisocialbackend.vercel.app'],
+  origin: ['http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Authorization'],
